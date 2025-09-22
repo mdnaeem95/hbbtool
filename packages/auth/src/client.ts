ec9f899a22a445f056c2ef09897a43451088fc2d
Naeem Sani <muhdnaeem95@gmail.com>
2025-09-22 09:44:33 +0800

    fix: relaxed typing on supabase browser client creation

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,7 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr'
-import type { SupabaseClient } from '@supabase/supabase-js'
 
-export function createBrowserSupabaseClient(): SupabaseClient {
+export function createBrowserSupabaseClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
